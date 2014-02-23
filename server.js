292ad98ad2cdd68b0cbd29916a10cf8501cc7d3b
sempers <groddenator@gmail.com>
2014-02-24 02:19:19 +0400

    NEWCOMMIT

--- server.js
+++ server.js
@@ -1,34 +1,36 @@
 var http = require('http');
 var express = require('express');
+var dblite = require('dblite');
 var search = require('./search');
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/public');
 app.engine('html', require('ejs').renderFile);
+app.use(express.methodOverride());
+app.set('port', process.env.PORT || 3000);
 
-var server = http.createServer(app);
 var dbCrashed = false;
 
 try {
-    var db = require('dblite')("vasmer.sqlite");
+	var db = dblite("vasmer.sqlite");
 }
-catch (e) {
-    dbCrashed = true;
+catch (e)
+{
+	dbCrashed = true;
 }
 
-app.get("/search/:word", function (req, res) {
+app.get("/search/:word", function(req, res){
     return search.searchWord(req, res, db);
 });
 
 app.get("/*", function (req, res) {
-    if (dbCrashed)
-        res.render("index-nodb.html");
-    else
-        res.render("index.html");
+	if (dbCrashed)
+		res.render("index-nodb.html");
+	else 
+		res.render("index.html");
 });
 
-server.listen(process.env.PORT || 3000, process.env.IP || "127.0.0.1", function () {
-    var addr = server.address();
-    console.log("Server listening at ", addr.address + ":" + addr.port);
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
